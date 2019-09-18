0afdd64bafd9391e1b32f2907f3ddd98ba7c9af0
Anjay <kumaranjay8@gmail.com>
2019-09-18 10:28:04 +0530

    ps1 completed

--- ps1.py
+++ ps1.py
@@ -18,17 +18,25 @@ else:
 print("Parity bit data:",i_data1)  # Printing The parity bit data
 o_data=[]
 j=0
-for items in i_data:
-    o_data.append(items)
-    #print(o_data)
-    o_data1="".join(o_data)
-    #print(o_data1)
+k=0
+o_data1=""
+for items in i_data1:                      #bit Stuffing Logic Starts Here
+    o_data1=o_data1+items
+
+
+
+
     o_data3=o_data1[j:]
-    print(o_data3)
+
     if '010' in o_data3:
-        print("hi")
-        #o_data2=o_data1+'0'
-        #o_data1=o_data2
-    j+=1
-        #print(o_data2)
-#print(o_data2)
+
+        o_data1=o_data1+'0'
+
+        j=k
+        j+=2
+        k+=1
+
+    k+=1
+
+o_datafinal=o_data1+'0101'                #Appending flag for end of the frame
+print("Transmitting Data is:",o_datafinal)    #printing The final data
